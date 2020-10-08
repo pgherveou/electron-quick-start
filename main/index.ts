98d992059d5f8a82f26883e08132735d7670451b
Pierre Guillaume Herveou <pgherveou@lyft.com>
2020-10-07 23:07:54 -0400

    wip

--- main/index.ts
+++ main/index.ts
@@ -1,6 +1,8 @@
-import { app, BrowserWindow } from 'electron'
+import { app } from 'electron'
+import { resolve } from 'path'
 import { createWindow } from './helper'
 
 console.log('launching app', process.argv)
+console.log('. is', resolve('.'))
 app.on('ready', createWindow)
 app.on('window-all-closed', () => app.quit())
